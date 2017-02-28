6c6788d15bb68c370aee947399e8a891cbefcfa8
Hui Jun <hjyam@umich.edu>
2017-02-28 15:11:43 -0800

    Complete user model

---
+++ app/models/user.rb
@@ -0,0 +1,43 @@
+class User < ActiveRecord::Base
+  attr_reader :password
+  validates :username, :email, :password_digest, :session_token, :name, presence: true
+  validates :username, :email, :session_token, uniqueness: true
+  validates :password, length: { minimum: 6 }, allow_nil: :true
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    return nil if user.nil?
+    user.is_password?(password) ? user : nil
+  end
+
+  def self.generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def reset_session_token
+    self.session_token = User.generate_session_token
+    ensure_session_token_uniqueness
+    self.save!
+    self.session_token
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def ensure_session_token_uniqueness
+    while User.find_by(session_token: self.session_token)
+      self.session_token = User.generate_session_token
+    end
+  end
+end
