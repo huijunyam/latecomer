aa9761a21c170e5f7e53bdc95a122d7ec9709b62
Hui Jun <hjyam@umich.edu>
2017-02-28 15:40:11 -0800

    Complete root.jsx

---
+++ frontend/components/root.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute, IndexRedirect, hashHistory } from 'react-router';
+
+import App from './app/app';
+import WelcomeContainer from './welcome/welcome_container';
+
+const Root = ({ store }) => {
+  const _ensureLoggedIn = (nextState, replace) => {
+    const currentUser = store.getState().session.currentUser;
+    if (!currentUser) {
+      replace('/');
+    }
+  };
+
+  const _redirectIfLoggedIn = (nextState, replace) => {
+    const currentUser = store.getState().session.currentUser;
+    if (currentUser) {
+      replace('/main');
+    }
+  };
+  return(
+    <Provider store={ store }>
+      <Router history= { hashHistory }>
+        <Route path="/" component= { App }>
+          <IndexRoute component={WelcomeContainer} onEnter={_redirectIfLoggedIn}/>
+
+        </Route>
+      </Router>
+    </Provider>
+  );
+};
+
+export default Root;
