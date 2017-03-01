5ac129d8d99d93634b439b2c5b44f8dffedb78c0
Hui Jun <hjyam@umich.edu>
2017-02-28 19:17:11 -0800

    Setup class controller

--- app/controllers/api/class_names_controller.rb
+++ app/controllers/api/class_names_controller.rb
@@ -1,2 +1,12 @@
 class Api::ClassNamesController < ApplicationController
+  def index
+    @class_names = ClassName.all
+    render :index
+  end
+
+  def show
+    @class_name = ClassName.find(params[:id])
+    render :show
+  end
+
 end
