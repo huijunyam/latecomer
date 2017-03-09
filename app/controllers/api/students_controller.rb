57940de9907c60a271548c46646cdef0892cdb4b
Hui Jun <hjyam@umich.edu>
2017-03-09 13:15:50 -0800

    Add create and destroy api to students

--- app/controllers/api/students_controller.rb
+++ app/controllers/api/students_controller.rb
@@ -8,4 +8,20 @@ class Api::StudentsController < ApplicationController
     @students = Student.all
     render :index
   end
+
+  def create
+    @student = Student.new(student_params)
+    if @student.save
+      render "api/students/show"
+    else
+      render json: @student.errors.full_messages, status: 422
+    end
+  end
+
+  def delete
+    student = Student.find(params[:id])
+    @class = student.class
+    student.destroy 
+    render "api/class_names/show"
+  end
 end
